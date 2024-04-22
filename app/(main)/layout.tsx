89391dad30f2f477013a223fc999e8933461a7a5
Yuji Tani <valleyyuji.webengineer@gmail.com>
2024-04-22 14:57:36 +0900

    learnsページ追加

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,19 @@
+import MobileSidebar from "@/components/ui/mobileSidebar";
+import { MobileHeader } from "@/components/ui/mobileHeader";
+import Sidebar from "@/components/ui/sidebar";
+
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <Sidebar className="hidden lg:flex" />
+      <MobileHeader className="lg:hidden fixed top-0 left-0 z-20" />
+      <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
+        <div className="bg-red-500 max-w-[1056px] mx-auto pt-6 h-full">
+          {children}
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default MainLayout;
