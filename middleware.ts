424f1ac35276916623f3a3bd728d7e5a4856cb82
Yuji Tani <valleyyuji.webengineer@gmail.com>
2024-04-24 21:37:11 +0900

    middlewareを更新

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextRequest, NextResponse } from "next/server";
 
 export default authMiddleware({
   publicRoutes: ["/", "/quests"],
@@ -7,3 +8,19 @@ export default authMiddleware({
 export const config = {
   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+
+export function middleware(request: NextRequest) {
+    // retrieve the current response
+    const res = NextResponse.next()
+
+    // add the CORS headers to the response
+    res.headers.append('Access-Control-Allow-Credentials', "true")
+    res.headers.append('Access-Control-Allow-Origin', '*') // replace this your actual origin
+    res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+    res.headers.append(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+
+    return res
+}
