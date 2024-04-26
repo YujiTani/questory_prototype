f02f70ecb48551db71b21377d93ccc149ef2a507
Yuji Tani <valleyyuji.webengineer@gmail.com>
2024-04-26 21:40:33 +0900

    複数のエリアを表示できるようにHTMLを修正した

--- app/(main)/sql/[id]/page.tsx
+++ app/(main)/sql/[id]/page.tsx
@@ -1,29 +1,49 @@
 export const runtime = "edge";
 
 import StageList from "@/components/ui/stageList";
-import { areas } from "@/data/areas";
-import { stages } from "@/data/stages";
+import { areas as areaData } from "@/data/areas";
+import { stages as stageData } from "@/data/stages";
 import Kanban from "@/components/ui/kanban";
 
+/**
+ * TODO: ページ課題
+ * area毎にコンポーネント区切れるように修正する (questで絞る)
+ * stageの取得方法も検討する (area_idで絞る)
+ * stage選択演出を用意する
+ * stageの表現が物足りない
+ * areaDataにオブジェクト画像をもたせる
+ */
 const SQLPage = ({ params }: { params: { id: string } }) => {
   // TODO: APIが出来たらFetch処理に変更する
-  const area = areas[0];
-  const stageData = stages;
+  const areas = areaData;
+  const stages = stageData;
 
   return (
     <>
-      <div className="flex justify-end">
-        <div className="w-3/4">
-          <Kanban
-            title={area.name}
-            subTitle={`Lv.${area.lv}`}
-            description={area.description}
-          />
+      {areas.length > 0 ? (
+        areas.map((area) => (
+          <div className="mt-10" key={area.id}>
+            <div className="flex justify-end">
+              <div className="w-3/4">
+                <Kanban
+                  title={area.name}
+                  subTitle={`Lv.${area.lv}`}
+                  description={area.description}
+                />
+              </div>
+            </div>
+            <div className="mt-10">
+              <StageList
+                stages={stages.filter((stage) => stage.area_id === area.id)}
+              />
+            </div>
+          </div>
+        ))
+      ) : (
+        <div>
+          <p>Not Found</p>
         </div>
-      </div>
-      <div className="mt-10">
-        <StageList stages={stageData} />
-      </div>
+      )}
     </>
   );
 };
