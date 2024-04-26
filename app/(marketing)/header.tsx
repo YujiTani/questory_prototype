3c19dc3a358bf80879e800a14f0891692bc8aea6
Yuji Tani <valleyyuji.webengineer@gmail.com>
2024-04-26 21:58:51 +0900

    ヘッダー画像で出ていた渓谷を解消

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -11,15 +11,16 @@ import Image from "next/image";
 
 export const Header = () => {
   return (
-    <header className="h-20 w-full border-b-2 border-slate-200">
+    <header className="h-20 w-full bg-white border-b-2 border-slate-200">
       <div className="lg:max-w-screen-lg pt-5 px-4 pb-4 mx-auto flex items-center justify-between h-full">
         <div className="flex items-center gap-3">
           <Image
-            className="rounded-full"
             src="/icon_yuzu.svg"
-            alt="logo"
-            width={60}
-            height={60}
+            alt="app_logo"
+            width="0"
+            height="0"
+            sizes="100vw"
+            className="w-16 h-auto rounded-full"
           />
           <h1 className="text-2xl font-extrabold text-green-700 tracking-wide">
             QueStory
