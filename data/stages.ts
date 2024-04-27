f99744c8bbfea880e1df859cc135567ec37fec93
Yuji Tani <valleyyuji.webengineer@gmail.com>
2024-04-28 00:19:13 +0900

    stageデータを修正

--- data/stages.ts
+++ data/stages.ts
@@ -1,5 +1,6 @@
 export type StageType = {
   id: number;
+  target: string;
   index: number;
   complete_case: number; //  1: "全問正解"
   failed_case: number; //  1: "同じ問題を5回間違える"
@@ -9,6 +10,7 @@ export type StageType = {
 export const stages = [
   {
     id: 1,
+    target: "正解の単語をえらんでください",
     index: 1,
     complete_case: 1,
     failed_case: 1,
@@ -16,6 +18,7 @@ export const stages = [
   },
   {
     id: 2,
+    target: "SQLをつくってみよう!(データ取得)",
     index: 2,
     complete_case: 1,
     failed_case: 1,
@@ -23,6 +26,7 @@ export const stages = [
   },
   {
     id: 3,
+    target: "SQLをつくってみよう!(データ更新)",
     index: 3,
     complete_case: 1,
     failed_case: 1,
@@ -30,6 +34,7 @@ export const stages = [
   },
   {
     id: 4,
+    target: "SQLをつくってみよう!(データの追加)",
     index: 4,
     complete_case: 1,
     failed_case: 1,
@@ -37,6 +42,7 @@ export const stages = [
   },
   {
     id: 5,
+    target: "正解の単語をえらんでください2",
     index: 5,
     complete_case: 1,
     failed_case: 1,
@@ -44,6 +50,7 @@ export const stages = [
   },
   {
     id: 6,
+    target: "SQLをつくってみよう!(データ更新2)",
     index: 6,
     complete_case: 1,
     failed_case: 1,
@@ -51,6 +58,7 @@ export const stages = [
   },
   {
     id: 7,
+    target: "正解の単語を選んでください3",
     index: 7,
     complete_case: 1,
     failed_case: 1,
@@ -58,6 +66,7 @@ export const stages = [
   },
   {
     id: 8,
+    target: "SQLをつくってみよう(データ取得2)",
     index: 8,
     complete_case: 1,
     failed_case: 1,
@@ -65,6 +74,7 @@ export const stages = [
   },
   {
     id: 9,
+    target: "SQLをつくってみよう!(データの追加2)",
     index: 9,
     complete_case: 1,
     failed_case: 1,
@@ -72,6 +82,7 @@ export const stages = [
   },
   {
     id: 10,
+    target: "正解の単語をえらんでください3",
     index: 10,
     complete_case: 1,
     failed_case: 1,
@@ -79,6 +90,7 @@ export const stages = [
   },
   {
     id: 11,
+    target: "テーブルをつなげてみよう(データ取得)",
     index: 11,
     complete_case: 1,
     failed_case: 1,
@@ -86,6 +98,7 @@ export const stages = [
   },
   {
     id: 12,
+    target: "テーブルをつなげてみよう(データ取得2)",
     index: 12,
     complete_case: 1,
     failed_case: 1,
