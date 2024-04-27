bc778822c3ee05861b190f360c4fc64db4e9a121
Yuji Tani <valleyyuji.webengineer@gmail.com>
2024-04-28 08:08:43 +0900

    問題文の改行ルールを変更

--- components/question/questionInfo.tsx
+++ components/question/questionInfo.tsx
@@ -12,7 +12,7 @@ const QuestionInfo = ({ target, title, index, count }: Props) => {
     <div className="mt-4 p-3 border-2 border-slate-400 rounded-md flex flex-col gap-3">
       <h1 className="text-sm font-bold">{target}</h1>
       <Separator orientation="horizontal" />
-      <p className="text-balance break-words">{title}</p>
+      <p className="text-pretty">{title}</p>
       <Separator orientation="horizontal" />
       <div className="flex justify-end">
         <span className="text-xs">
