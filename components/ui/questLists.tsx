8f05e129cd3ba273050dfd5ee51f15c62748fcb0
Yuji Tani <valleyyuji.webengineer@gmail.com>
2024-04-27 11:48:54 +0900

    インデントの修正

--- components/ui/questLists.tsx
+++ components/ui/questLists.tsx
@@ -9,34 +9,33 @@ type Props = {
 
 const QuestLists = ({ quests }: Props) => {
   function lowerString(questName: string) {
-    return questName.toLowerCase()
+    return questName.toLowerCase();
   }
 
   return (
-      <div className="mt-5 flex flex-wrap gap-4 justify-center">
-        {/* QuestListコンポーネントに切り出す  */}
-        {quests &&
-          quests.map((quest) => (
-            <Link
-              key={quest.id}
-              href={`/${lowerString(quest.name)}/1`}
-              className="flex flex-col items-center"
-            >
-              <QuestCard>
-                <Image
-                  className="rounded border-2 border-slate-200 p-1"
-                  src={quest.image || "/NotData.svg"}
-                  alt={`${quest.name}のicon`}
-                  width={110}
-                  height={70}
-                />
-                <span>{quest.name}</span>
-              </QuestCard>
-            </Link>
-          ))}
-      </div>
+    <div className="mt-5 flex flex-wrap gap-4 justify-center">
+      {/* QuestListコンポーネントに切り出す  */}
+      {quests &&
+        quests.map((quest) => (
+          <Link
+            key={quest.id}
+            href={`/${lowerString(quest.name)}/1`}
+            className="flex flex-col items-center"
+          >
+            <QuestCard>
+              <Image
+                className="rounded border-2 border-slate-200 p-1"
+                src={quest.image || "/NotData.svg"}
+                alt={`${quest.name}のicon`}
+                width={110}
+                height={70}
+              />
+              <span>{quest.name}</span>
+            </QuestCard>
+          </Link>
+        ))}
+    </div>
   );
 };
 
 export default QuestLists;
-
