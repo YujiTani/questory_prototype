2811ea123678a7576e31b362f0bb37db44794a55
Yuji Tani <valleyyuji.webengineer@gmail.com>
2024-04-28 00:19:29 +0900

    questionデータも修正

--- data/questions.ts
+++ data/questions.ts
@@ -1,13 +1,5 @@
 export type questionType = "sorting" | "select";
 
-export type sortingQuestion = {
-  id: number;
-  question: string;
-  answer: string;
-  stage_id: number;
-  type: "sorting";
-};
-
 export type selectQuestion = {
   id: number;
   question: string;
@@ -18,114 +10,13 @@ export type selectQuestion = {
   type: "select";
 };
 
-// 選択しながら並べる問題
-const sortingQuestions = [
-  {
-      id: 1,
-      question: "IDが1のユーザーの名前を取得する",
-      answer: "SELECT name FROM users WHERE id = 1;",
-      stage_id: 2,
-      type: "sorting",
-  },
-  {
-    id: 2,
-    question: "IDが3のユーザーの年齢を取得する",
-    answer: "SELECT age FROM users WHERE id = 3;",
-    stage_id: 2,
-    type: "sorting",
-  },
-  {
-    id: 3,
-    question: "IDが1のユーザーの登録日を取得する",
-    answer: "SELECT created_at FROM users WHERE id = 1;",
-    stage_id: 2,
-    type: "sorting",
-  },
-  {
-    id: 4,
-    question: "IDが1のユーザーの名前と年齢と登録日を取得する",
-    answer: "SELECT name, age, created_at FROM users WHERE id = 1;",
-    stage_id: 2,
-    type: "sorting",
-  },
-  {
-    id: 5,
-    question: "全ユーザーの名前を取得する",
-    answer: "SELECT name FROM users;",
-    stage_id: 2,
-    type: "sorting",
-  },
-  {
-    id: 6,
-    question: "全ユーザーの全ての情報を取得する",
-    answer: "SELECT * FROM users;",
-    stage_id: 2,
-    type: "sorting",
-  },
-  {
-    id: 7,
-    question: "名前が登録されていないユーザーを全員を取得する",
-    answer: "SELECT * FROM users WHERE name IS NULL;",
-    stage_id: 2,
-    type: "sorting",
-  },
-  {
-    id: 8,
-    question: "名前が登録されているユーザー全員を取得する",
-    answer: "SELECT * FROM users WHERE name IS NOT NULL;",
-    stage_id: 2,
-    type: "sorting",
-  },
-  {
-    id: 9,
-    question: "年齢が登録されていないユーザー全員の名前を取得する",
-    answer: "SELECT name FROM users WHERE age IS NULL;",
-    stage_id: 2,
-    type: "sorting",
-  },
-  {
-    id: 10,
-    question: "名前と年齢が登録されているユーザー全員の名前を取得する",
-    answer: "SELECT name FROM users WHERE name IS NOT NULL AND age IS NOT NULL;",
-    stage_id: 2,
-    type: "sorting",
-  },
-  {
-    id: 11,
-    question: "ユーザーの総数を取得する",
-    answer: "SELECT COUNT(*) FROM users;",
-    stage_id: 2,
-    type: "sorting",
-  },
-  {
-    id: 12,
-    question: "最も古い登録日のユーザーの名前を取得する",
-    answer: "SELECT name FROM users ORDER BY created_at ASC LIMIT 1;",
-    stage_id: 2,
-    type: "sorting",
-  },
-  {
-    id: 13,
-    question: "年齢が20歳以上30歳以下のユーザーの名前を取得する",
-    answer: "SELECT name FROM users WHERE age BETWEEN 20 AND 30;",
-    stage_id: 2,
-    type: "sorting",
-  },
-  {
-    id: 14,
-    question: "最後に登録された3人のユーザーの名前を取得する",
-    answer: "SELECT name FROM users ORDER BY created_at DESC LIMIT 3;",
-    stage_id: 2,
-    type: "sorting",
-  },
-  {
-    id: 15,
-    question: "名前が'A'で始まるユーザーの全情報を取得する",
-    answer: "SELECT * FROM users WHERE name LIKE 'A%';",
-    stage_id: 2,
-    type: "sorting",
-  }
-] as const satisfies sortingQuestion[]
+export type sortingQuestion = {
+  id: number;
+  question: string;
+  answer: string;
+  stage_id: number;
+  type: "sorting";
+};
 
 const selectQuestions = [
   {
@@ -277,11 +168,117 @@ const selectQuestions = [
   },
 ] as const satisfies selectQuestion[]
 
-
-export const questions = [
+// 選択しながら並べる問題
+const sortingQuestions = [
   {
-    sortingQuestions,
-    selectQuestions,
+      id: 1,
+      question: "IDが1のユーザーの名前を取得する",
+      answer: "SELECT name FROM users WHERE id = 1;",
+      stage_id: 2,
+      type: "sorting",
+  },
+  {
+    id: 2,
+    question: "IDが3のユーザーの年齢を取得する",
+    answer: "SELECT age FROM users WHERE id = 3;",
+    stage_id: 2,
+    type: "sorting",
+  },
+  {
+    id: 3,
+    question: "IDが1のユーザーの登録日を取得する",
+    answer: "SELECT created_at FROM users WHERE id = 1;",
+    stage_id: 2,
+    type: "sorting",
+  },
+  {
+    id: 4,
+    question: "IDが1のユーザーの名前と年齢と登録日を取得する",
+    answer: "SELECT name, age, created_at FROM users WHERE id = 1;",
+    stage_id: 2,
+    type: "sorting",
+  },
+  {
+    id: 5,
+    question: "全ユーザーの名前を取得する",
+    answer: "SELECT name FROM users;",
+    stage_id: 2,
+    type: "sorting",
+  },
+  {
+    id: 6,
+    question: "全ユーザーの全ての情報を取得する",
+    answer: "SELECT * FROM users;",
+    stage_id: 2,
+    type: "sorting",
+  },
+  {
+    id: 7,
+    question: "名前が登録されていないユーザーを全員を取得する",
+    answer: "SELECT * FROM users WHERE name IS NULL;",
+    stage_id: 2,
+    type: "sorting",
+  },
+  {
+    id: 8,
+    question: "名前が登録されているユーザー全員を取得する",
+    answer: "SELECT * FROM users WHERE name IS NOT NULL;",
+    stage_id: 2,
+    type: "sorting",
+  },
+  {
+    id: 9,
+    question: "年齢が登録されていないユーザー全員の名前を取得する",
+    answer: "SELECT name FROM users WHERE age IS NULL;",
+    stage_id: 2,
+    type: "sorting",
+  },
+  {
+    id: 10,
+    question: "名前と年齢が登録されているユーザー全員の名前を取得する",
+    answer: "SELECT name FROM users WHERE name IS NOT NULL AND age IS NOT NULL;",
+    stage_id: 2,
+    type: "sorting",
+  },
+  {
+    id: 11,
+    question: "ユーザーの総数を取得する",
+    answer: "SELECT COUNT(*) FROM users;",
+    stage_id: 2,
+    type: "sorting",
+  },
+  {
+    id: 12,
+    question: "最も古い登録日のユーザーの名前を取得する",
+    answer: "SELECT name FROM users ORDER BY created_at ASC LIMIT 1;",
+    stage_id: 2,
+    type: "sorting",
+  },
+  {
+    id: 13,
+    question: "年齢が20歳以上30歳以下のユーザーの名前を取得する",
+    answer: "SELECT name FROM users WHERE age BETWEEN 20 AND 30;",
+    stage_id: 2,
+    type: "sorting",
+  },
+  {
+    id: 14,
+    question: "最後に登録された3人のユーザーの名前を取得する",
+    answer: "SELECT name FROM users ORDER BY created_at DESC LIMIT 3;",
+    stage_id: 2,
+    type: "sorting",
+  },
+  {
+    id: 15,
+    question: "名前が'A'で始まるユーザーの全情報を取得する",
+    answer: "SELECT * FROM users WHERE name LIKE 'A%';",
+    stage_id: 2,
+    type: "sorting",
   }
+] as const satisfies sortingQuestion[]
+
+export const questionsList = [
+  {id: 1, questions: selectQuestions},
+  {id: 2, questions: sortingQuestions},
 ]
 
