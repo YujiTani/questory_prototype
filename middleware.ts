e9ccebd78aa186663fd87b6e5e8788ba19ab312e
Yuji Tani <valleyyuji.webengineer@gmail.com>
2024-04-30 23:09:43 +0900

    env設定とpublicrootを変更

--- middleware.ts
+++ middleware.ts
@@ -4,18 +4,8 @@ import { authMiddleware } from "@clerk/nextjs";
 const sqlPath = ["/sql/:id", "/sql/stage/:id"];
 
 export default authMiddleware({
-  beforeAuth: (res) => {
-    res.headers.append('Access-Control-Allow-Credentials', "true")
-    res.headers.append('Access-Control-Allow-Origin', '*') // replace this your actual origin
-    res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
-    res.headers.append(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-  },
-
-
-  publicRoutes: ["/", "/quests", ...sqlPath, "active_record/:id", "ruby/:id", ],
+  publicRoutes: ["/"]
+  // "/quests", ...sqlPath, "active_record/:id", "ruby/:id", ],
 });
 
 export const config = {
