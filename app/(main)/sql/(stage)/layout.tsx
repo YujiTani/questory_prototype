64dbd7ccc3372f754693c55fd679545b841d8d87
Yuji Tani <valleyyuji.webengineer@gmail.com>
2024-05-01 05:35:39 +0900

    幅変更あとでちゃんとやる

--- app/(main)/sql/(stage)/layout.tsx
+++ app/(main)/sql/(stage)/layout.tsx
@@ -6,12 +6,12 @@ import { MAX_WIDTH } from "@/data/content";
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <StageHeader className="lg:hidden fixed top-0 left-0 z-20" />
-      <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
-        <div className={`${MAX_WIDTH} mx-auto p-2 h-full tracking-wide`}>
-          {children}
-        </div>
-      </main>
+      <StageHeader className="fixed top-0 left-0 z-20 h-[50px]" />
+      <div
+        className={`pt-[50px] md:w-[50%] max-w[600px] mx-auto p-2 h-full tracking-wide`}
+      >
+        {children}
+      </div>
     </>
   );
 };
